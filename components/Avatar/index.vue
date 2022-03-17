21e6aa0e54792214cdeb89123d5b6a82ea5a1d2b
Xianwen Yu <xianwenyu@livetrade.com.cn>
2022-03-17 15:34:53 +0800

    增加了 Avatar 头像

---
+++ components/Avatar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-avatar width="40" height="40" min-width="40">
+    <v-img v-if="src" :src="src" @error="imgError" />
+    <v-icon v-else size="40" color="gray1">mdi-account-circle</v-icon>
+  </v-avatar>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    src: String
+  },
+  setup() {
+    const imgError = () => {
+      this.src = false;
+    };
+    return {
+      imgError
+    };
+  }
+});
+</script>
